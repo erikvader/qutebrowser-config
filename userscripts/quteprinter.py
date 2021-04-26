2f7523353ac23a313f714c98762db223727867af
Erik Rimskog <erik.rimskog@gmail.com>
2021-04-27 00:53:14 +0200

    qute_utils

--- userscripts/quteprinter.py
+++ userscripts/quteprinter.py
@@ -1,14 +1,43 @@
+# more like quteutils
+
 import os
 
-def _qute_print(msg, cmd):
+
+def send_to_qute(msg):
     with open(os.environ["QUTE_FIFO"], "w") as f:
-        f.write("{} '{}'\n".format(cmd, msg.translate("".maketrans("", "", "\"'"))))
+        f.write("{}\n".format(msg))
+
+
+def qute_print_cmd(msg, cmd):
+    send_to_qute("{} '{}'".format(cmd, msg.translate("".maketrans("", "", "\"'"))))
+
 
 def qute_print(msg):
-    _qute_print(msg, "message-info")
+    qute_print_cmd(msg, "message-info")
+
 
 def qute_eprint(msg):
-    _qute_print(msg, "message-error")
+    qute_print_cmd(msg, "message-error")
+
 
 def qute_wprint(msg):
-    _qute_print(msg, "message-warning")
+    qute_print_cmd(msg, "message-warning")
+
+
+def qute_jseval(js, quiet=True):
+    send_to_qute("jseval {}{}".format("-q " if quiet else "", js))
+
+
+def get_qute_page():
+    from bs4 import BeautifulSoup
+
+    with open(os.environ["QUTE_HTML"], "r") as html_file:
+        return BeautifulSoup(html_file, "lxml")
+
+
+def started_from_qute():
+    return (
+        "QUTE_FIFO" in os.environ
+        and "QUTE_HTML" in os.environ
+        and "QUTE_URL" in os.environ
+    )
